83d0e18ff2167a52eea259e4fcaae99d9a4ff615
mercyio <mercydanke@gmail.com>
2024-02-14 14:48:23 +0100

    hot fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PetModule } from 'src/Pets/pets.module';
+import { PetModule } from '../Pets/pets.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { AuthGuard } from './guard/auth.guard';
  import { ConfigModule, ConfigService } from '@nestjs/config';
-import { PetEntity } from 'src/entities/pets.entity';
+import { PetEntity } from '../entities/pets.entity';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './guard/roles.guard';
 import { ProfileEntity } from '../entities/profile.entity';
-import { ReviewEntity } from 'src/entities/review.entity';
-import { OrderEntity } from 'src/entities/order.entity';
+import { ReviewEntity } from '../entities/review.entity';
+import { OrderEntity } from '../entities/order.entity';
 // import { UserModule } from '../user/user.module';
 
 
